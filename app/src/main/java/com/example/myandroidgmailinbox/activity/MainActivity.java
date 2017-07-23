7aa30ee6212cf1a7f863330cba661d9c72f4571a
DarushDev <darushfaqiri@gmail.com>
2017-07-23 16:58:35 +0800

    created packages

--- app/src/main/java/com/example/myandroidgmailinbox/activity/MainActivity.java
+++ app/src/main/java/com/example/myandroidgmailinbox/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myandroidgmailinbox;
+package com.example.myandroidgmailinbox.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.myandroidgmailinbox.R;
 
 public class MainActivity extends AppCompatActivity {
 
